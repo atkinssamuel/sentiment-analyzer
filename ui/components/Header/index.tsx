a7e77e7bad5490e8992ac661d06846d184984b45
Samuel Atkins <s.m.atkins73@gmail.com>
2024-10-15 23:21:36 +0000

    Adds base UI components

---
+++ ui/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+  return (
+    <div className={`max-width row-flex space-between`}>
+      <div>Title</div>
+      <div>Right text</div>
+    </div>
+  )
+}
